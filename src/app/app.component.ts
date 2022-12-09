9ea2bfdbb007de327274d8d8955a2dffb01d9185
Yanzen <yanwarsolah@gmail.com>
2022-12-10 06:42:10 +0700

    Menambahkan HttpClient dan beberapa method untuk get postingan

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { catchError, throwError } from 'rxjs';
+import { PostInterface } from './interfaces/post-interface';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-hands-on';
+  posts: PostInterface[];
+  baseURL: string = "https://jsonplaceholder.typicode.com";
+
+  constructor(private http: HttpClient) {
+    this.posts = [];
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, body was: `, error.error);
+    }
+    // Return an observable with a user-facing error message.
+    return throwError(() => new Error('Something bad happened; please try again later.'));
+  }
+
+  public getPost() {
+    this.http.get<PostInterface[]>(`${this.baseURL}/posts`)
+      .pipe(catchError(this.handleError))
+      .subscribe((response: PostInterface[]) => {
+        this.posts = response;
+      })
+  }
 }
